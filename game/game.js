bada118bc23c3bb85a0b45cbf4415dff2dd9a6e3
Joao Juliatti <joaopjuliatti@gmail.com>
2020-02-19 19:42:17 -0300

    adding gravity and jump

---
+++ game/game.js
@@ -0,0 +1,181 @@
+//Initialization canvas and contex
+const globalCanvas = document.createElement('canvas');
+globalCanvas.width = 1000;
+globalCanvas.height = 500;
+document.getElementById('game-board').appendChild(globalCanvas);
+
+const globalContex1 = globalCanvas.getContext('2d');
+
+
+class Game{
+    constructor(canvas){
+        this.canvas = canvas;
+        this.contex = canvas.getContext('2d');
+        this.playerInitX = 0
+        this.playerInitY = 400
+        this.atomization = 500
+        this.playerWidth = 20
+        this.playerHeigth = 40
+        this.gravity = 0.05
+    }
+    start = () =>{
+        this.player = new Player(this.playerInitX,this.playerInitY,0,0,this.playerWidth,this.playerHeigth,this.canvas)
+        this.gameArea = new GameArea(this.canvas,this.playerInitY+this.playerHeigth,500)
+        this.gameArea.start()
+        this.player.start()
+    }
+    clear = () =>{
+        this.contex.clearRect(0,0,this.canvas.width,this.canvas.height)
+    }
+    updateGame = () =>{
+        this.clear()
+        this.gameArea.drawGameArea()
+        this.player.drawPlayer(0,this.gravity)
+
+    }
+    interval = () =>{
+        setInterval(this.updateGame,20)
+    }
+    checkGroundOrTop = () =>{
+        return this.gameArea.terrains.reduce((acc,terrain)=>{
+            return acc || terrain.checkIfTouch(this.player)
+        },false)
+    }   
+    
+}
+
+class GameArea{
+    constructor(canvas,initY,atomization){
+        this.canvas = canvas;
+        this.contex = canvas.getContext('2d');
+        this.atomization = atomization;
+        this.terrains = [];
+        this.initY = initY;
+    }
+    start = () =>{
+        const widthAtom = this.canvas.width/this.atomization
+        const numberRows = (this.canvas.height - this.initY)/widthAtom
+        for(let i = 0;i<this.atomization;i++){
+            for(let j=0; j<numberRows;j++){
+                this.terrains.push(new Terrain(0+i*widthAtom,this.initY+j*widthAtom,0,0,widthAtom+1,widthAtom+1,this.canvas))
+            }
+        }
+        this.drawGameArea()
+    }
+    drawGameArea = () =>{
+        this.contex.fillStyle = "black"
+        this.contex.fillRect(0,0,1000,500);
+        for(let i = 0;i<this.terrains.length;i++){
+            this.terrains[i].draw();
+        }
+    }
+
+}
+
+
+
+
+class Component{
+    constructor(x,y,Vx,Vy,width,height,canvas){
+        this.x = x;
+        this.y = y;
+        this.Vx = Vx;
+        this.Vy = Vy;
+        this.ax = 0;
+        this.ay = 0;
+        this.width = width;
+        this.height = height;
+        this.canvas = canvas;
+        this.contex = canvas.getContext('2d')
+        this.left = this.x;
+        this.right = this.x+this.width
+        this.top = this.y
+        this.bottom = this.y+ this.height
+    }
+}
+
+
+class Terrain extends Component{
+    constructor(x,y,Vx,Vy,width,height,canvas){
+        super(x,y,Vx,Vy,width,height,canvas);
+    }
+    draw = () =>{
+        this.contex.fillStyle = 'yellow'
+        this.contex.fillRect(this.x,this.y,this.width,this.height);
+    }
+    checkIfTouch = (player) =>{
+        return (
+          player.top() > this.bottom() ||
+          player.bottom() < this.top()
+        )  
+      }
+}
+
+
+
+class Player extends Component{
+    constructor(x,y,Vx,Vy,width,height,canvas){
+        super(x,y,Vx,Vy,width,height,canvas);
+    }
+    start = () =>{
+        this.drawPlayer(0,0)
+    }
+    drawPlayer = (ax,ay) =>{
+        this.newAceleration(ax,ay)
+        this.contex.fillStyle = 'red'
+        this.contex.fillRect(this.x,this.y,this.width,this.height);   
+    }
+    newPos = () =>{
+        this.x += this.Vx
+        this.y += this.Vy
+    }
+    newVelocity = () =>{
+        this.Vx += this.ax
+        this.Vy += this.ay
+        this.newPos()  
+    }
+    newAceleration = (ax,ay) =>{
+        this.ay = ay;
+        this.ax = ax;
+        this.newVelocity()
+    }
+
+}
+
+game = new Game(globalCanvas)
+
+game.start()
+
+game.interval()
+
+
+
+//player movement
+document.onkeydown = function(e){
+    switch (e.keyCode) {
+      case 37:
+        game.player.Vx=-5
+        break;
+      case 39:
+        game.player.Vx=+5
+        break;
+    case 32:
+        game.player.Vy=-2
+        break;     
+    }
+  }
+
+  document.onkeyup = (e) =>{
+    switch (e.keyCode) {
+        case 37:
+          game.player.Vx = 0
+          break;
+        case 39:
+          game.player.Vx = 0
+          break;      
+      }
+      
+  }
+
+
+
