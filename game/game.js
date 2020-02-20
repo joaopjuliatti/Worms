23d37fdc208cac7b014fcb4efe6760bda328d296
Joao Juliatti <joaopjuliatti@gmail.com>
2020-02-19 22:19:53 -0300

    add attack and bullets class

--- game/game.js
+++ game/game.js
@@ -1,18 +1,10 @@
 //player movement
-map = {}
+keysUsed = {}
 document.onkeydown = function(e){
-    map[e.keyCode] = e.type =='keydown'
-    console.log(map)
-    if(map[37]) game.player.Vx=-1
-    if(map[39]) game.player.Vx= 1
-    if(map[32]) game.player.Vy=-2
+    keysUsed[e.keyCode] = e.type =='keydown'
   }
   document.onkeyup = function(e){
-    map[e.keyCode] = e.type =='keydown'
-    console.log(map)
-    if(map[37]) game.player.Vx=-1
-    if(map[39]) game.player.Vx= 1
-    if(map[32]) game.player.Vy=-2
+    keysUsed[e.keyCode] = e.type =='keydown'
 }
 
 
@@ -49,11 +41,15 @@ class Game{
     clear = () =>{
         this.contex.clearRect(0,0,this.canvas.width,this.canvas.height)
     }
+    keysPressed = () =>{
+        if(keysUsed[37]) game.player.Vx=-1
+        if(keysUsed[39]) game.player.Vx= 1
+        if(keysUsed[32]) game.player.Vy=-2
+        if(keysUsed[90]) game.player.attack()
+    }
     updateGame = () =>{
-        if(map[37]) game.player.Vx=-1
-        if(map[39]) game.player.Vx= 1
-        if(map[32]) game.player.Vy=-2
         this.clear()
+        this,this.keysPressed()
         this.gameArea.drawGameArea()
         this.player.newPos(0,this.gravity)
         if(this.checkIfTouchY(this.player)){
@@ -78,7 +74,7 @@ class Game{
         this.player.drawPlayer()
     }
     interval = () =>{
-        setInterval(this.updateGame,2)
+        setInterval(this.updateGame,1)
     }
     checkIfTouchY = (player) =>{
         const teste =this.gameArea.terrains.reduce((acc,terrain)=>{
@@ -136,6 +132,8 @@ class Component{
         this.height = height;
         this.canvas = canvas;
         this.contex = canvas.getContext('2d')
+        this.frames = 0;
+        this.gravity = 0;
     }
     left = () =>{
         return this.x;
@@ -148,6 +146,22 @@ class Component{
     }
     bottom = () =>{
         return this.y+ this.height
+    }
+    newPos = (ax,ay) =>{
+        this.previousX = this.x
+        this.previousY = this.y
+        this.x += this.Vx
+        this.y += this.Vy
+        this.newVelocity(ax,ay)
+    }
+    newVelocity = (ax,ay) =>{
+        this.Vx += this.ax
+        this.Vy += this.ay
+        this.newAceleration(ax,ay)
+    }
+    newAceleration = (ax,ay) =>{
+        this.ay = ay;
+        this.ax = ax;
     }
 }
 
@@ -169,10 +183,12 @@ class Terrain extends Component{
 }
 
 
-
 class Player extends Component{
     constructor(x,y,Vx,Vy,width,height,canvas){
         super(x,y,Vx,Vy,width,height,canvas);
+        this.bullets = [];
+        this.gravity = 0.1
+        this.numberBullets = 3
     }
     start = () =>{
         this.newPos(0,0)
@@ -180,25 +196,41 @@ class Player extends Component{
     }
     drawPlayer = () =>{
         this.contex.fillStyle = 'red'
-        this.contex.fillRect(this.x,this.y,this.width,this.height);   
+        this.contex.fillRect(this.x,this.y,this.width,this.height);
+        this.frames++;   
+        this.drawBullets()  
+        if(this.frames===500) this.frames = 0
     }
-    newPos = (ax,ay) =>{
-        this.previousX = this.x
-        this.previousY = this.y
-        this.x += this.Vx
-        this.y += this.Vy
-        this.newVelocity(ax,ay)
+    attack = () =>{
+        console.log(this.frames)
+        if(this.frames % 50 ===0 && this.bullets>0){
+            this.bullets.push(new Bullet(this.x,this.y,10,-2,20,10,this.canvas))
+            this.bullets--
+        }
     }
-    newVelocity = (ax,ay) =>{
-        this.Vx += this.ax
-        this.Vy += this.ay
-        this.newAceleration(ax,ay)
-    }
-    newAceleration = (ax,ay) =>{
-        this.ay = ay;
-        this.ax = ax;
+    drawBullets =() =>{
+        for(let i = 0;i<this.bullets.length;i++){
+            this.bullets[i].newPos(0,this.gravity)
+            this.bullets[i].drawBullet()
+        }
     }
 
+}
+
+class Bullet extends Component{
+    constructor(x,y,Vx,Vy,width,height,canvas){
+        super(x,y,Vx,Vy,width,height,canvas);
+    }
+    drawBullet = () =>{
+        this.contex.fillStyle = 'pink'
+        this.contex.fillRect(this.x,this.y,this.width,this.height);
+    }
+    // checkGroundOrTop = (player) =>{
+    //     return (
+    //         ( player.bottom() > this.top() && player.bottom() < this.bottom())||
+    //         ( player.top() < this.bottom() && this.top() < player.top() )
+    //     )  
+    //   }
 }
 
 game = new Game(globalCanvas)
